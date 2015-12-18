0444a217d39211f2d265aad5a09de765817a1e90
Pritish Sai <Pritish.sai@gmail.com>
2015-12-19 01:22:39 +0530

    user and article associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
- validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 25 }
+ has_many :articles
+ before_save { self.email = email.downcase }
+ validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
- validates :email, presence: true, uniqueness: true, length: { minimum: 5, maximum: 105 }, format: { with: VALID_EMAIL_REGEX }
+ validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 5, maximum: 105 }, format: { with: VALID_EMAIL_REGEX }
 end
